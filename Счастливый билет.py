5f1ef3157112f45abc32845ac31929e32774ef2b
user <liberty_gg@mail.ru>
2023-12-14 22:16:55 +0300

    Короткий вариант

--- Счастливый билет.py
+++ Счастливый билет.py
@@ -1,4 +1,5 @@
-ticket_number = input('Введите номер билета (6 цифр): ')
+ticket_number = [int(i) for i in input('Введите номер билета (6 цифр): ')]
+"""
 tn_lst = list(ticket_number)
 
 num1 = tn_lst[0]
@@ -18,6 +19,11 @@ sum_first_3_nums = int(num1_int+num2_int+num3_int)
 sum_second_3_nums = int(num4_int+num5_int+num6_int)
 
 if sum_first_3_nums == sum_second_3_nums:
+    print("Билет счастливый!")
+else:
+    print("НЕ В ЭТОТ РАЗ")
+"""
+if sum(ticket_number[:3]) == sum(ticket_number[3:]):
     print("Билет счастливый!")
 else:
     print("НЕ В ЭТОТ РАЗ")
