6f317e4b3dbb4fa1af997a22b7a03c071d5401bf
user <liberty_gg@mail.ru>
2023-12-18 00:17:40 +0300

    P1_5B

--- P1_5B/P1_5B_3.py
+++ P1_5B/P1_5B_3.py
@@ -3,13 +3,14 @@ y = 2
 def squaring(x,y):
     sq = x**y
     return sq
-
+"""Возведение в степень."""
 def squaring_rec(x,y):
     if y == 1:
         return(x)
     if y != 1:
         sq_rec = x * squaring_rec(x, y-1)
         return sq_rec
+"""Возведение в степень с использованием рекурсии."""
 
 S1 = squaring(num,y)
 print(S1)
