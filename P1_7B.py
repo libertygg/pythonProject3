1404982d65ba5e918ea7035eb0a8e0e5bc23a6c1
user <liberty_gg@mail.ru>
2023-12-27 22:54:10 +0300

    P1_7B

--- P1_7B.py
+++ P1_7B.py
@@ -1,42 +1,27 @@
 from random import randint
 
-def comp_numb():
-    n_comp = randint(1, 100)
-    print(n_comp)
-    print('Компьютер "загадал" число в интервале от 1 до 100. Какое?')
-    return n_comp
+n_comp = randint(1, 100)
+print(n_comp)
+print('Компьютер "загадал" число в интервале от 1 до 100. Какое?')
 
-def attempt(func):
-    def numb_of_attempt(u_n, c_n, nf):
-        global n
-        nf += 1
-        n = nf
-        return_value = func(u_n, c_n, nf)
-        return return_value
-    return numb_of_attempt
+def att(func):
+    func.data = 0
+    return func
 
-@attempt
-def check_numder(u_n, c_n, nf):
-    print("Попытка:", nf)
-    if u_n > c_n:
+@att
+def check_numder(numder, n_comp):
+    check_numder.data += 1
+    if numder > n_comp:
         return 'Загаданное число меньше'
-    elif u_n < c_n:
+    elif otvet < n_comp:
         return 'Загаданное число больше.'
     else:
-        return "Правильно! Число попыток отгадывания:", nf
-    return return_value
+        return f'Правильно! Число попыток отгадывания - {check_numder.data}'
 
-def us_numb():
-    while True:
-        x = int(input('Введите Ваше число: '))
-        return x
+while True:
 
-c_n = comp_numb()
-u_n = 0
-n = 0
-while c_n != u_n:
-
-    u_n = us_numb()
-    print(u_n)
-    check = check_numder(u_n, c_n, n)
-    print(check)
+    otvet = int(input('Введите Ваше число '))
+    answer = check_numder(otvet, n_comp)
+    print(answer)
+    if otvet == n_comp:
+        break
